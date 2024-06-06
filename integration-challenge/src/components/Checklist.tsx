fe8b5736e4e9621b389d78de0e7bc12900e6931a
Vishnu Chakroborty <vishnuchakro@gmail.com>
2024-06-05 21:56:01 -0500

    initial checklist ui

---
+++ integration-challenge/src/components/Checklist.tsx
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from "react";
+import styles from "@/styles/Checklist.module.css";
+import { useRouter } from "next/router";
+import {
+  ChevronRightIcon,
+  RadioButtonIcon,
+  RadioButtonCheckedIcon,
+  RadioButtonOutlinedIcon,
+} from "../assets/icons/IconLibrary";
+
+type OnboardingItemProps = {
+  id: string;
+  title: string;
+  checked?: boolean;
+  focused?: boolean;
+  onClick: () => any;
+};
+
+type OnboardingCheckboxProps = { checked?: boolean; focused?: boolean };
+
+const OnboardingCheckbox = ({
+  checked,
+  focused = false,
+}: OnboardingCheckboxProps) => {
+  if (focused)
+    return (
+      <RadioButtonOutlinedIcon width={24} height={24} className="text-main" />
+    );
+  if (checked)
+    return (
+      <RadioButtonCheckedIcon
+        width={24}
+        height={24}
+        className="text-gray-100"
+      />
+    );
+
+  return <RadioButtonIcon width={24} height={24} className="text-gray-300" />;
+};
+
+const OnboardingItem = ({
+  id,
+  title,
+  checked,
+  focused,
+  onClick,
+}: OnboardingItemProps) => {
+  return (
+    <div
+      className={`${styles.nextStepsStep + " " + styles.nextStepsStepPointer}`}
+      id={id}
+      onClick={onClick}
+    >
+      <div>
+        <OnboardingCheckbox checked={checked} focused={focused} />
+        <span>{title}</span>
+      </div>
+      <div>
+        {!checked && (
+          <ChevronRightIcon width={12} height={12} className="text-main" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default function Checklist() {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(getOnboardingList());
+  }, []);
+
+  const getOnboardingList = () => {
+    const onboardingList: {
+      id: string;
+      checked?: boolean;
+      focused?: boolean;
+      title: string;
+      onClick: () => any;
+    }[] = [
+      {
+        id: "fund",
+        checked: false,
+        title: "Make a deposit into your account",
+        onClick: () => {},
+      },
+      {
+        id: "scheduleMeeting",
+        checked: false,
+        title: "Schedule a meeting with your wealth manager",
+        onClick: () => {},
+      },
+      {
+        id: "accessDigitalBanking",
+        checked: false,
+        title: "Access online banking",
+        onClick: () => {},
+      },
+      {
+        id: "addNewChecking",
+        checked: false,
+        title: "Add a checking account",
+        onClick: () => {},
+      },
+      {
+        id: "requestDebit",
+        checked: false,
+        title: "Request a debit card",
+        onClick: () => {},
+      },
+      {
+        id: "addBeneficiary",
+        checked: false,
+        title: "Add a beneficiary",
+        onClick: () => {},
+      },
+      {
+        id: "openShare",
+        checked: false,
+        title: "Open a certificate of deposit",
+        onClick: () => {},
+      },
+      {
+        id: "connectDirectDeposit",
+        checked: false,
+        title: "Add/Switch Direct Deposit",
+        onClick: () => {},
+      },
+      {
+        id: "openAccount",
+        checked: false,
+        title: "Add an account",
+        onClick: () => {},
+      },
+    ];
+
+    // Highlight radio button based on sequence
+    const firstUnchecked = onboardingList.find((item) => !item.checked);
+    if (firstUnchecked) firstUnchecked.focused = true;
+
+    return onboardingList;
+  };
+
+  return (
+    <div className={`${styles.nextStepsSteps} ${styles.nextStepStepsMinWidth}`}>
+      {getOnboardingList().map(({ id, title, checked, focused, onClick }) => {
+        return (
+          <OnboardingItem
+            key={id}
+            id={id}
+            checked={checked}
+            focused={focused}
+            title={title}
+            onClick={onClick}
+          />
+        );
+      })}
+    </div>
+  );
+}
--- integration-challenge/tailwind.config.ts
+++ integration-challenge/tailwind.config.ts
@@ -8,10 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        grey: {
+          100: "#f3f4f6",
+        },
+        tahiti: {
+          100: "#cffafe",
+          200: "#a5f3fc",
+          300: "#67e8f9",
+          400: "#22d3ee",
+          500: "#06b6d4",
+          600: "#0891b2",
+          700: "#0e7490",
+          800: "#155e75",
+          900: "#164e63",
+        },
+        // ...
       },
     },
   },
